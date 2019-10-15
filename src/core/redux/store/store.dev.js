5ff47b3b790a01b0628aebfb42f257bf09e80545
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-10-15 19:33:11 -0300

    remove (dependencies) redux-devtools-extension

--- src/core/redux/store/store.dev.js
+++ src/core/redux/store/store.dev.js
@@ -1,14 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore, persistReducer } from 'redux-persist';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 import AsyncStorage from '@react-native-community/async-storage';
 
 import reducers from 'ducks';
 import rootSaga from 'sagas';
-
-const composeEnhancers = composeWithDevTools({});
 
 const sagaMiddleware = createSagaMiddleware({
   sagaMonitor: console.tron.createSagaMonitor(),
@@ -25,7 +22,10 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = createStore(
   persistedReducer,
-  composeEnhancers(applyMiddleware(...middlewares)),
+  compose(
+    console.tron.createEnhancer(),
+    applyMiddleware(...middlewares),
+  ),
 );
 
 const persistor = persistStore(store);

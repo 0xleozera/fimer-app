65cc183955bccf197f53f2bdafff1c853a956ada
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-10-17 02:02:29 -0300

    fix (component) rename to keep default

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,6 +1,6 @@
-import Background from './Background';
-import Button from './Button';
-import DateField from './DateField';
-import TextField from './TextField';
+import Background from './background';
+import Button from './button';
+import DateField from './date-field';
+import TextField from './text-field';
 
 export { Background, Button, DateField, TextField };

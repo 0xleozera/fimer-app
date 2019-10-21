f329c94fc4bd1e06a2bdfff5d492f6e04621c830
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-10-21 20:55:47 -0300

    add (project) change color android navigation bar

--- src/app/screens/app/index.js
+++ src/app/screens/app/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AppState } from 'react-native';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 import { useSelector } from 'react-redux';
 
 import createRouter from 'routes';
 import Navigator from 'routes/navigator';
 
-const App = () => {
+import { withTheme } from 'styled-components';
+
+const App = ({ theme }) => {
   const authenticated = useSelector(state => state.auth.token);
   const Routes = createRouter(!!authenticated);
+
+  useEffect(() => {
+    function setAndroidNavigationBar() {
+      changeNavigationBarColor(theme.colors.primary.dark);
+    }
+
+    function handleAppStateChange(nextAppState) {
+      if (nextAppState === 'active') {
+        setAndroidNavigationBar();
+      }
+    }
+
+    setAndroidNavigationBar();
+
+    AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
+  }, [theme.colors.primary.dark]);
 
   return <Routes ref={ref => Navigator.setNavigator(ref)} />;
 };
 
-export default App;
+export default withTheme(App);

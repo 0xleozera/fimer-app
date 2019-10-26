98c7719d91a4c5b9d1ad2e2d61c021b6841ffe42
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-10-26 20:26:56 -0300

    fix (configs) api url change to work with android emulator and reactotron

--- src/core/services/api/index.js
+++ src/core/services/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { outbound, inbound } from 'utils/interceptors';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://192.168.0.102:3333',
 });
 
 api.interceptors.request.use(request => outbound(request));

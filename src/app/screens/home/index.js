e9c8b58a1cde8e85e41d85285d0d2b5c936ca882
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-09 02:40:40 -0300

    fix (home) nickname on welcome message

--- src/app/screens/home/index.js
+++ src/app/screens/home/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import useTheme from 'hooks/use-theme';
 
+import { useSelector } from 'react-redux';
+
 import { BaseScreen, Typography } from 'components';
 import { HeaderHome, Logo, ContainerGreet, Greet } from './styles';
 
@@ -9,6 +11,7 @@ import List from './list';
 
 const Home = () => {
   const theme = useTheme();
+  const nickname = useSelector(state => state.auth.user.nickname);
 
   return (
     <BaseScreen statusBarBackground={theme.colors.primary.dark}>
@@ -18,7 +21,7 @@ const Home = () => {
       <ContainerGreet>
         <Greet>
           <Typography size="h4" font="bold" color="contrast">
-            Fala tu, Darfus!
+            Fala tu, {nickname}!
           </Typography>
         </Greet>
         <Typography size="h6" font="medium" color="contrast">

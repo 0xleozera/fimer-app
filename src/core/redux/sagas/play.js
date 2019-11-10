cafeb1ead416105bdc068d4658acb8a729df7cb2
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-10 04:18:20 -0300

    add (play) new request after changing filter

--- src/core/redux/sagas/play.js
+++ src/core/redux/sagas/play.js
@@ -2,13 +2,35 @@ import api from 'api';
 import { URLBuilder } from 'utils/url-builder';
 
 import { Alert } from 'react-native';
-import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all, select } from 'redux-saga/effects';
 
 import { Creators as PlayActions, Types as PlayTypes } from 'ducks/play';
+import { Types as FilterTypes } from 'ducks/filters';
 
-export function* getPlayers(action) {
+export function* getPlayers() {
   try {
-    const queryParams = URLBuilder(action.payload);
+    const { data } = yield call(api.get, 'search');
+    yield put(PlayActions.getPlaySuccess(data));
+  } catch (err) {
+    Alert.alert(
+      'Falha ao carregar',
+      'Houve um erro ao carregar a lista de jogadores',
+    );
+    yield put(PlayActions.getPlayFailure());
+  }
+}
+
+export function* getFilteringPlayers() {
+  try {
+    const [game, position, ranking, region, gender] = yield all([
+      select(state => state.filters.game.selected),
+      select(state => state.filters.position.selected),
+      select(state => state.filters.ranking.selected),
+      select(state => state.filters.region.selected),
+      select(state => state.filters.gender.selected),
+    ]);
+
+    const queryParams = URLBuilder({ game, position, ranking, region, gender });
     const { data } = yield call(api.get, `search${queryParams}`);
     yield put(PlayActions.getPlaySuccess(data));
   } catch (err) {
@@ -20,4 +42,7 @@ export function* getPlayers(action) {
   }
 }
 
-export default all([takeLatest(PlayTypes.GET_PLAY_REQUEST, getPlayers)]);
+export default all([
+  takeLatest(PlayTypes.GET_PLAY_REQUEST, getPlayers),
+  takeLatest(FilterTypes.SET_FILTER, getFilteringPlayers),
+]);

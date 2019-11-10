6e66a15bbd9f54491b9c0415dcecfd8a7b3dd98c
Leonardo Pinheiro <leonardo.pinheiro@proenem.com.br>
2019-11-10 17:57:31 -0300

    fix (play) remove unused

--- src/app/screens/play/players/styles.js
+++ src/app/screens/play/players/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
-import { PlayActionButton, Typography } from 'components';
+import { PlayActionButton } from 'components';
 
 import {
   getViewportDimension,
